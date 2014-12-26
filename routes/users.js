11111b00df177f2110e6152bc397f8064e8eebc2
alearnaiz <alejandro.arnaiz.gonzalez@gmail.com>
2014-12-26 01:06:27 +0100

    Test creating an user

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+// GET /users
 router.get('/', function(req, res) {
   res.send('respond with a resource');
+});
+
+// POST /users
+router.post('/', function(req, res){
+  var username = req.body.username;
+  var password = req.body.password;
+  console.log(username + ' ' + password);
+  res.end();
 });
 
 module.exports = router;
