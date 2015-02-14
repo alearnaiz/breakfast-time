7a97fc82bb43ea315f0a812fa3559f14bb79e6cb
alearnaiz <alejandro.arnaiz.gonzalez@gmail.com>
2015-02-14 22:07:05 +0100

    Error solved

--- routes/breakfasts.js
+++ routes/breakfasts.js
@@ -23,7 +23,7 @@ router.post('/:username/breakfasts', function(req, res) {
 // GET /users/:username/breakfasts/active
 router.get('/:username/breakfasts/active', function(req, res) {
     var connection = mysql.connect();
-    connection.query('SELECT food.name, drink.name FROM breakfast LEFT JOIN food ON breakfast.food_id = food.id LEFT JOIN drink ON breakfast.drink_id = drink.id WHERE user_username = ? AND endDate IS NULL',
+    connection.query('SELECT food.name AS foodName, drink.name AS drinkName FROM breakfast LEFT JOIN food ON breakfast.food_id = food.id LEFT JOIN drink ON breakfast.drink_id = drink.id WHERE user_username = ? AND endDate IS NULL',
         [req.params.username], function(err, rows) {
             if (err) {
                 throw err;
